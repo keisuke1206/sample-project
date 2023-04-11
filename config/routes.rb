f591b36aebd42147bc4d2b743c108ae68b973753
keisuke1206 <keisukeeee12061206@gmail.com>
2023-04-12 00:57:16 +0900

    ルートについて

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html 
   # Defines the root path route ("/")
-    root "sample#top_page"
+    get 'sample/top_page', to: 'sample#top_page'
+    get 'sample/login', to: 'sample#login'
 end
